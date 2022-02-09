8e39b45b45a0fe52464c360147595995439173b6
vesavvo <vesa.ollikainen@metropolia.fi>
2022-02-09 17:03:57 +0200

    Lisätty pelin ID

--- Demopeli/pelaaja.py
+++
@@ -1,3 +0,0 @@
-class Pelaaja:
-    def __init__(self, nick):
-        self.nick = nick
--- Demopeli/peli.py
+++ Demopeli/peli.py
@@ -1,43 +1,7 @@
-from pelaaja import Pelaaja
-from kenttä import Kenttä
-from säätila import Säätila
-import json
+import random
 
-def etsi_lähikentät(sijainti, max_xdist, max_ydist):
-    lista = []
-    # DUMMY - korvataan tietokantahaulla
-    lista.append(Kenttä("TKU"))
-    lista.append(Kenttä("TMP"))
-    lista.append(Kenttä("MIK"))
-    return lista
-
-def tee_datapaketti(nick, raha, sijainti, kentät_tarjolla):
-    kohteet = []
-    for k in kentät_tarjolla:
-        kohteet.append((k.__dict__))
-    data = {
-        "nick" : nick,
-        "raha" : raha,
-        "sijainti" : sijainti.__dict__,
-        "säätila": säätila.__dict__,
-        "tarjolla" : kohteet,
-    }
-
-    json_data = json.dumps(data)
-    return json_data
-
-# Alusta peli
-nick = "Vesa"
-alkusijainti = "HEL"
-raha = 1500
-max_xdist = 1
-max_ydist = 1
-
-pelaaja = Pelaaja(nick)
-sijainti = Kenttä(alkusijainti)
-säätila = Säätila(sijainti)
-
-kentät_tarjolla = etsi_lähikentät(sijainti, max_xdist, max_ydist)
-
-json_data = tee_datapaketti(nick, raha, sijainti, kentät_tarjolla)
-print(json_data)
+class Peli:
+    def __init__(self, nick, raha):
+        self.id = str(random.randint(1,100000000))+str(random.randint(1,100000000))
+        self.raha = raha
+        self.nick = nick
