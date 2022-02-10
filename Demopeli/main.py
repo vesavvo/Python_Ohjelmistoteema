5e6572e1f30d931bf5671a973883399d35fa7771
vesavvo <vesa.ollikainen@metropolia.fi>
2022-02-10 16:08:38 +0200

    Säätila sijainnin sisällä JSONissa

--- Demopeli/main.py
+++ Demopeli/main.py
@@ -5,30 +5,27 @@ import json, random, string
 
 initial_money = 1500
 
-def make_json(peli, sijainti, säätila, kohteet):
-    kohdelista = []
-    for k in kohteet:
-        kohdelista.append((k.__dict__))
+def make_json(peli, sijainti, kohteet):
     data = {
-        "game" : peli.__dict__,
-        "location" : sijainti.__dict__,
-        "weather": säätila.__dict__,
-        "possible_destinations" : kohdelista
+        "game" : peli,
+        "location" : sijainti,
+        "possible_destinations" : kohteet
     }
-
-    json_data = json.dumps(data)
+    print(data)
+    json_data = json.dumps(data, default=lambda o: o.__dict__, indent=4)
+    print(json_data)
     return json_data
 
 def fly(id, dest):
 
     game = Game(id)
     sijainti = Airport(dest)
-    säätila = Weather(sijainti)
+    sijainti.getWeather()
     game.updateLocation(sijainti)
 
     kohteet = sijainti.find_nearby_airports()
 
-    json_data = make_json(game, sijainti, säätila, kohteet)
+    json_data = make_json(game, sijainti, kohteet)
 
     return json_data
 
