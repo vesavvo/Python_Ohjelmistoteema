daa36a0d1e7d6cafc4c11487f582aae7ba2f9f3c
Vesa Ollikainen <vesa.ollikainen@metropolia.fi>
2022-02-11 15:15:02 +0200

    Update maria.sql

--- Demopeli/Tietokanta/maria.sql
+++ Demopeli/Tietokanta/maria.sql
@@ -27,3 +27,28 @@ LOAD DATA LOCAL INFILE "C:\\Users\\olliv\\Documents\\airports\\airports.csv"
 INTO TABLE Airport
 CHARACTER SET 'utf8'
 FIELDS TERMINATED BY "," ENCLOSED BY '"';
+
+
+DROP TABLE IF EXISTS Goal;
+
+CREATE TABLE Goal (
+	id INTEGER,
+	name VARCHAR(40),
+	description VARCHAR(200),
+	target VARCHAR(40),
+	target_minvalue DECIMAL(8,2),
+	target_maxvalue DECIMAL(8,2),
+	target_text VARCHAR(40),
+	PRIMARY KEY (id)
+);
+
+INSERT INTO Goal VALUES 
+(1, "HOT", "Temperature over +25C", "TEMP", 25, 9999, NULL),
+(2, "COLD", "Temperature under -25C", "TEMP", -9999, -25, NULL),
+(3, "0DEG", "Temperature exactly 0C", "TEMP", -0.5, 0.5, NULL),
+(4, "10DEG", "Temperature exactly +10C", "TEMP", 9.5, 10.5, NULL),
+(5, "20DEG", "Temperature exactly +20C", "TEMP", 19.5, 20.5, NULL),
+(6, "SUNNY", "Sunny weather", "WEATHER", NULL, NULL, "Sunny"),
+(7, "CLOUDS", "Cloudy weather", "WEATHER", NULL, NULL, "Clouds"),
+(8, "WINDY", "Wind blows more than 10 m/s", "WIND", 10.0, 9999, NULL)
+;
