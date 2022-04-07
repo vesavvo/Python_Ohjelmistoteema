16e81b307d82a1ea90244384249801e8c2df1c3d
vesavvo <vesa.ollikainen@metropolia.fi>
2022-04-07 16:38:38 +0300

    Ulkoiset tietolähteet aloitettu

---
+++ Esimerkit/apipyyntö.py
@@ -0,0 +1,12 @@
+import requests
+
+hakusana = input("Anna hakusana: ")
+
+# Pyynnön malli: https://api.tvmaze.com/search/shows?q=girls
+pyyntö = "https://api.tvmaze.com/search/shows?q=" + hakusana
+vastaus = requests.get(pyyntö).json()
+print(vastaus)
+
+for a in vastaus:
+    print(a)
+
